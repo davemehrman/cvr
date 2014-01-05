169946f60c2166a1668470db9c84be55360698c6
Dave Mehrman <dmehrman@gmail.com>
2014-01-05 16:04:16 -0600

    Adding Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cvr::Application.config.secret_key_base = 'e9e4985c74dee94c6b6f55ee92d9c7bb0fc628e51b06d1eb07881f51320ab834b3e005d52f4302a22cc3001639ea78c315e036441ef095299ac538102d30eed5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cvr::Application.config.session_store :cookie_store, key: '_cvr_session'
